eb3697532b2ae5d8033fc717a1e663b9f4500d23
anaaclara07 <clararts38@gmail.com>
2024-06-13 10:01:27 -0300

    ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import React,{useState} from 'react'
+import {useState} from 'react'
 import {Routes,Route} from 'react-router-dom' 
 import Home from './pages/Home'
 
+
+
 const App = () => {
-
-
   const [color] = useState("#eb8d04")
   document.body.style.backgroundColor = color
 
@@ -13,8 +13,7 @@ const App = () => {
   return(
     <Routes>
       <Route path="/" element={<Home/>} />
-      <Route path='/inicio' element={<Inicio/>} />
-    </Routes>
+      </Routes>
   )
 }
 
